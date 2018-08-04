15d241f545aa542d89f5ea075684b8ee83ada66e
Abraham Schilling <6810177+n4bb12@users.noreply.github.com>
2018-08-04 17:36:11 +0200

    Add npm config

--- src/bash/partials/npm.sh
+++ src/bash/partials/npm.sh
@@ -9,3 +9,11 @@ require-binary "npm"
 require-node-package "philemon"
 
 alias npm="philemon npm"
+
+npm config set color always
+npm config set editor code
+npm config set git-tag-version false
+npm config set init.author.email "AbrahamSchilling@gmail.com"
+npm config set init.author.name "Abraham Schilling"
+npm config set progress false
+npm config set shell bash
