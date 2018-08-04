e24f31dd4f1f29bf5b79b18ebe67b8cd6ed901b4
Abraham Schilling <6810177+n4bb12@users.noreply.github.com>
2018-08-04 17:07:56 +0200

    Add git chmod alias

--- src/bash/partials/git.sh
+++ src/bash/partials/git.sh
@@ -7,17 +7,14 @@
 
 require-binary "git"
 
-git-msg() {
-  git log -1 --pretty=%B
-}
-
 git-chmod() {
   git update-index --chmod="$1"
 }
 
 alias gm="git-msg"
 
-git config --global alias.amend "commit --ammend --no-edit"
+git config --global alias.amend "commit --amend --no-edit"
+git config --global alias.chmod "update-index --chmod"
 git config --global alias.fix "commit --fixup"
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git config --global push.followTags true
