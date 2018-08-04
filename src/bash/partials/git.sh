0a87abf62d06cdeb0697e9fa4a319d9f04c716a3
Abraham Schilling <6810177+n4bb12@users.noreply.github.com>
2018-08-04 13:59:46 +0200

    Add global git config

--- src/bash/partials/git.sh
+++ src/bash/partials/git.sh
@@ -16,3 +16,8 @@ git-chmod() {
 }
 
 alias gm="git-msg"
+
+git config --global alias.amend "commit --ammend --no-edit"
+git config --global alias.fix "commit --fixup"
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global push.followTags true
