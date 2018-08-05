d130db54b72ddb33ed5c88c4670edec725ecfb4b
Abraham Schilling <6810177+n4bb12@users.noreply.github.com>
2018-08-05 22:45:29 +0200

    add account urls & sort urls

--- src/bash/partials/urls.sh
+++ src/bash/partials/urls.sh
@@ -1,12 +1,35 @@
 #
 #
+#   Profile Pages
+#
+# ==========================================================
+id() {
+  args="$@"
+  command="$1"
+  shift
+  rest="$@"
+
+  if false; then echo false
+  elif [ "$command" == "gh" ]; then
+    id github "$rest"
+  elif [ "$command" == "github" ]; then
+    browser 'https://github.com/n4bb12' "$rest"
+  elif [ "$command" == "gitlab" ]; then
+    browser 'https://gitlab.com/n4bb12' "$rest"
+  elif [ "$command" == "gl" ]; then
+    id gitlab "$rest"
+  else
+    echo Unknown id "$args"
+  fi
+}
+#
+#
 #   Google Search
 #   https://www.lifewire.com/advanced-google-search-3482174
 #
 # ==========================================================
 search() { browser 'https://www.google.de/search?q=' "$@"; }
 alias s="search"
-alias calc="s"
 alias define="s define"
 alias google="s"
 alias stopwatch="s stopwatch"
@@ -15,21 +38,28 @@ alias watch="s watch"
 alias weather="s weather"
 #
 #
-#   Websites
+#   Other Search
 #
 # ==========================================================
 caniuse() { browser 'https://caniuse.com/#search=' "$@"; }
-dango() { browser 'https://getdango.com/interactive/?banner=0' "$@"; }
+langs() { browser 'http://ionicabizau.github.io/github-profile-languages/?user=' "$@"; }
 emoji() { browser 'http://emoji.muan.co/#' "$@"; }
 alias emo="emoji"
-escape() { browser 'http://www.theukwebdesigncompany.com/articles/entity-escape-characters.php' "$@"; }
 flaticon() { browser 'https://www.flaticon.com/search?word=' "$@"; }
-github() { browser 'https://github.com/n4bb12/' "$@"; }
-gitlab() { browser 'https://gitlab.com/n4bb12/' "$@"; }
 maps() { browser 'https://www.google.de/maps/search/' "$@"; }
-music() { browser 'https://music.youtube.com/' "$@"; }
-youtube() { browser 'https://www.youtube.com/' "$@"; }
-alias yt="youtube"
+#
+#
+#   Websites
+#
+# ==========================================================
+contacts() { browser 'https://contacts.google.com' "$@"; }
+dango() { browser 'https://getdango.com/interactive/?banner=0' "$@"; }
+escape() { browser 'http://www.theukwebdesigncompany.com/articles/entity-escape-characters.php' "$@"; }
+github() { browser 'https://github.com/n4bb12' "$@"; }
+gitlab() { browser 'https://gitlab.com/n4bb12' "$@"; }
+mail() { browser 'https://mail.google.com' "$@"; }
+music() { browser 'https://music.youtube.com' "$@"; }
+youtube() { browser 'https://www.youtube.com' "$@"; }
 #
 #
 #   References
@@ -43,29 +73,63 @@ reference() {
 
   if false; then echo false
   elif [ "$command" == "angular" ]; then
-    browser 'https://github.com/angular/angular-cli/wiki/' "$rest"
+    browser 'https://github.com/angular/angular-cli/wiki' "$rest"
   elif [ "$command" == "cypress" ]; then
-    browser 'https://yarnpkg.com/en/docs/cli/' "$rest"
+    browser 'https://docs.cypress.io' "$rest"
+  elif [ "$command" == "enzyme" ]; then
+    https://airbnb.io/enzyme/docs/api/ "$rest"
   elif [ "$command" == "icons" ]; then
     browser 'https://material.io/tools/icons/?search=' "$rest"
+  elif [ "$command" == "insect" ]; then
+    browser 'https://github.com/sharkdp/insect#reference' "$rest"
+  elif [ "$command" == "jest" ]; then
+    https://jestjs.io/docs/en/getting-started "$rest"
   elif [ "$command" == "lodash" ]; then
     browser 'https://lodash.com/docs#' "$rest"
   elif [ "$command" == "ng" ]; then
-    browser 'https://github.com/angular/angular-cli/wiki/' "$rest"
+    reference angular "$rest"
   elif [ "$command" == "node" ]; then
     browser 'https://nodejs.org/api/index.html' "$rest"
   elif [ "$command" == "npm" ]; then
-    browser 'https://docs.npmjs.com/cli/' "$rest"
+    browser 'https://docs.npmjs.com/cli' "$rest"
   elif [ "$command" == "package" ]; then
     browser 'https://docs.npmjs.com/files/package.json#' "$rest"
   elif [ "$command" == "pkg" ]; then
-    browser 'https://docs.npmjs.com/files/package.json#' "$rest"
+    reference package "$rest"
+  elif [ "$command" == "react" ]; then
+    browser 'https://reactjs.org/docs/getting-started.html' "$rest"
+  elif [ "$command" == "sc" ]; then
+    reference styled "$rest"
   elif [ "$command" == "selectors" ]; then
     browser 'https://www.w3schools.com/cssref/css_selectors.asp' "$rest"
+  elif [ "$command" == "styled" ]; then
+    browser 'https://www.styled-components.com/docs' "$rest"
+  elif [ "$command" == "tsconfig" ]; then
+    browser 'https://www.typescriptlang.org/docs/handbook/compiler-options.html' "$rest"
+  elif [ "$command" == "tslint" ]; then
+    browser 'https://palantir.github.io/tslint/rules' "$rest"
   elif [ "$command" == "types" ]; then
     browser 'https://microsoft.github.io/TypeSearch' "$rest"
   elif [ "$command" == "yarn" ]; then
-    browser 'https://yarnpkg.com/en/docs/cli/' "$rest"
+    browser 'https://yarnpkg.com/en/docs/cli' "$rest"
+  else
+    search 'reference docs' "$args"
+  fi
+}
+#
+#
+#   Tools
+#
+# ==========================================================
+tool() {
+  args="$@"
+  command="$1"
+  shift
+  rest="$@"
+
+  if false; then echo false
+  elif [ "$command" == "angular" ]; then
+    browser 'https://github.com/angular/angular-cli/wiki' "$rest"
   else
     search reference docs "$args"
   fi
@@ -73,3 +137,10 @@ reference() {
 alias ref="reference"
 alias man="ref"
 alias docs="ref"
+#
+#
+#   Misc
+#
+# ==========================================================
+insect() { browser 'https://insect.sh/?q=' "$@"; }
+alias calc="insect"
--- src/bash/urls.ts
+++ src/bash/urls.ts
@@ -6,12 +6,20 @@ function generateUrls() {
   const script = new BashScript()
   const { alias, browse, section } = script
 
+  section("Profile Pages")
+
+  script.switch("id", "echo Unknown id", {
+    gh: "id github",
+    github: "browser 'https://github.com/n4bb12'",
+    gitlab: "browser 'https://gitlab.com/n4bb12'",
+    gl: "id gitlab",
+  })
+
   section("Google Search",
-    "https://www.lifewire.com/advanced-google-search-3482174")
+  "https://www.lifewire.com/advanced-google-search-3482174")
   browse("search", "https://www.google.de/search?q=")
   const search = alias("s", "search")
 
-  alias("calc", search())
   alias("define", search("define"))
   alias("google", search())
   alias("stopwatch", search("stopwatch"))
@@ -19,39 +27,63 @@ function generateUrls() {
   alias("watch", search("watch"))
   alias("weather", search("weather"))
 
-  section("Websites")
+  section("Other Search")
 
   browse("caniuse", "https://caniuse.com/#search=")
-  browse("dango", "https://getdango.com/interactive/?banner=0")
+  browse("langs", "http://ionicabizau.github.io/github-profile-languages/?user=")
   browse("emoji", "http://emoji.muan.co/#").alias("emo")
-  browse("escape", "http://www.theukwebdesigncompany.com/articles/entity-escape-characters.php")
   browse("flaticon", "https://www.flaticon.com/search?word=")
-  browse("github", "https://github.com/n4bb12/")
-  browse("gitlab", "https://gitlab.com/n4bb12/")
   browse("maps", "https://www.google.de/maps/search/")
-  browse("music", "https://music.youtube.com/")
-  browse("youtube", "https://www.youtube.com/").alias("yt")
+
+  section("Websites")
+
+  browse("contacts", "https://contacts.google.com")
+  browse("dango", "https://getdango.com/interactive/?banner=0")
+  browse("escape", "http://www.theukwebdesigncompany.com/articles/entity-escape-characters.php")
+  browse("github", "https://github.com/n4bb12")
+  browse("gitlab", "https://gitlab.com/n4bb12")
+  browse("mail", "https://mail.google.com")
+  browse("music", "https://music.youtube.com")
+  browse("youtube", "https://www.youtube.com")
 
   section("References")
 
-  script.switch("reference", "search reference docs", {
-    angular:   "browser 'https://github.com/angular/angular-cli/wiki/'",
-    cypress:   "browser 'https://yarnpkg.com/en/docs/cli/'",
+  script.switch("reference", "search 'reference docs'", {
+    angular:   "browser 'https://github.com/angular/angular-cli/wiki'",
+    cypress:   "browser 'https://docs.cypress.io'",
+    enzyme:    "https://airbnb.io/enzyme/docs/api/",
     icons:     "browser 'https://material.io/tools/icons/?search='",
+    insect:    "browser 'https://github.com/sharkdp/insect#reference'",
+    jest:      "https://jestjs.io/docs/en/getting-started",
     lodash:    "browser 'https://lodash.com/docs#'",
-    ng:        "browser 'https://github.com/angular/angular-cli/wiki/'",
+    ng:        "reference angular",
     node:      "browser 'https://nodejs.org/api/index.html'",
-    npm:       "browser 'https://docs.npmjs.com/cli/'",
+    npm:       "browser 'https://docs.npmjs.com/cli'",
     package:   "browser 'https://docs.npmjs.com/files/package.json#'",
-    pkg:       "browser 'https://docs.npmjs.com/files/package.json#'",
+    pkg:       "reference package",
+    react:     "browser 'https://reactjs.org/docs/getting-started.html'",
+    sc:        "reference styled",
     selectors: "browser 'https://www.w3schools.com/cssref/css_selectors.asp'",
+    styled:    "browser 'https://www.styled-components.com/docs'",
+    tsconfig:  "browser 'https://www.typescriptlang.org/docs/handbook/compiler-options.html'",
+    tslint:    "browser 'https://palantir.github.io/tslint/rules'",
     types:     "browser 'https://microsoft.github.io/TypeSearch'",
-    yarn:      "browser 'https://yarnpkg.com/en/docs/cli/'",
+    yarn:      "browser 'https://yarnpkg.com/en/docs/cli'",
+  })
+
+  section("Tools")
+
+  script.switch("tool", "search reference docs", {
+    angular:   "browser 'https://github.com/angular/angular-cli/wiki'",
   })
 
   const ref = alias("ref", "reference")
   alias("man", ref())
   alias("docs", ref())
+
+  section("Misc")
+
+  browse("insect", "https://insect.sh/?q=").alias("calc")
 
   script.outputTo(__dirname, "partials", "urls.sh")
 }
