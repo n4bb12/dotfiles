3bc1665912bcf4ba6772c06fd70a05850f922ed8
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-08-15 03:04:33 +0200

    git push.default current

--- src/init/git.sh
+++ src/init/git.sh
@@ -9,5 +9,5 @@ git config --global core.autocrlf false
 git config --global core.editor "code --wait"
 git config --global diff.tool "vs-code"
 git config --global difftool.cmd "code --wait --diff $LOCAL $REMOTE"
-git config --global push.default simple
+git config --global push.default current
 git config --global push.followTags true
