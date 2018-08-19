c2f840e0c6b7481d0f453938a49e96605156b39b
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-08-19 23:08:44 +0200

    Improve cd aliases

--- src/bash/partials/fs.sh
+++ src/bash/partials/fs.sh
@@ -13,7 +13,11 @@
 
 alias   ~='cd ~'
 alias  ..='cd ..'
-alias ...='cd ../..'
+alias  ..1='cd ..'
+alias  ..2='cd ../..'
+alias  ..3='cd ../../..'
+alias  ..4='cd ../../../..'
+alias  ..5='cd ../../../../..'
 
 # Create a new directory and enter it
 function mkd() {
