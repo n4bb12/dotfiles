7acfa580733ee171c820ac0caabb96b4b89c6a36
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-08-26 16:11:30 +0200

    Add lots of tools

--- src/bash/partials/bitly.sh
+++ src/bash/partials/bitly.sh
@@ -1,13 +1,11 @@
-#  
-# 
+#
+#
 #   Bitly CLI
 #   https://github.com/specious/bitly-client
-# 
+#
 # ================================================
 
-require-node-package 'bitly-client'   
+require-node-package 'bitly-client'
 
 alias      ly='bitly-client'
-alias     bit='bitly-client'
-alias   bitly='bitly-client'
 alias shorten='bitly-client'
--- src/bash/partials/web-tools.sh
+++ src/bash/partials/web-tools.sh
@@ -3,5 +3,38 @@
 #   Tools
 #
 # ==========================================================
+bitly() { browser 'https://bitly.com' "$@"; }
+codepad() { browser 'http://codepad.org' "$@"; }
+convert() { browser 'https://www.media.io' "$@"; }
+doodle() { browser 'https://doodle.com/create' "$@"; }
+favicomatic() { browser 'http://www.favicomatic.com' "$@"; }
+alias favicons='favicomatic'
+alias appicons='favicomatic'
+filedropper() { browser 'https://www.filedropper.com' "$@"; }
+forms() { browser 'https://docs.google.com/forms' "$@"; }
+alias poll='forms'
+gist() { browser 'https://gist.github.com' "$@"; }
+iframely() { browser 'http://iframely.com/debug?uri=' "$@"; }
+alias preview='iframely'
 insect() { browser 'https://insect.sh/?q=' "$@"; }
+alias calculator='insect'
 alias calc='insect'
+mydevice() { browser 'https://www.mydevice.io/' "$@"; }
+paste() { browser 'https://paste2.org' "$@"; }
+postbin() { browser 'http://postb.in' "$@"; }
+pwned() { browser 'https://haveibeenpwned.com' "$@"; }
+rmbg() { browser 'http://editphotosforfree.com/photoapps/remove-background-from-image-online' "$@"; }
+slides() { browser 'https://slides.google.com' "$@"; }
+speedtest() { browser 'http://speedtest.t-online.de' "$@"; }
+alias speed='speedtest'
+spritegen() { browser 'http://css.spritegen.com' "$@"; }
+alias sprites='spritegen'
+strawpoll() { browser 'https://www.strawpoll.me' "$@"; }
+alias straw='strawpoll'
+tineye() { browser 'https://labs.tineye.com/color' "$@"; }
+tinypng() { browser 'https://tinypng.com' "$@"; }
+alias panda='tinypng'
+toolbox() { browser 'https://richardstoolbox.com' "$@"; }
+wolframalpha() { browser 'http://www.wolframalpha.com/input/?i=' "$@"; }
+alias wolfram='wolframalpha'
+alias alpha='wolframalpha'
