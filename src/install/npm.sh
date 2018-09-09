572f9e8e85f1cf447dddddd283323cdc8d616309
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-09-09 20:11:53 +0200

    Change npm config

--- src/install/npm.sh
+++ src/install/npm.sh
@@ -1,12 +1,12 @@
-#  
-# 
+#
+#
 #   npm config
 #   https://docs.npmjs.com/misc/config
-# 
+#
 # ================================================
 
 npm config set color always
 npm config set editor code
-npm config set git-tag-version false
-npm config set progress false
+npm config set git-tag-version true
+npm config set progress true
 npm config set shell bash
