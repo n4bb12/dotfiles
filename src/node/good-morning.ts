6e93a704ac8e00333c6c4ae6e1e62fa624e9793e
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-09-20 00:26:41 +0200

    Add script for recursively fetching and pruning git repositories

---
+++ src/node/good-morning.ts
@@ -0,0 +1,49 @@
+import chalk from "chalk"
+import { lstatSync, readdirSync } from "fs"
+import Nehemiah from "nehemiah"
+import { basename, join, relative } from "path"
+
+function searchRepositories(dir: string): string[] {
+  const results: string[] = []
+  const subs = getSubDirectories(dir)
+  const subNames = subs.map(sub => basename(sub))
+
+  if (subNames.includes(".git")) {
+    results.push(dir)
+  } else {
+    subs.forEach(sub => {
+      searchRepositories(sub).forEach(result => results.push(result))
+    })
+  }
+
+  return results
+}
+
+function getSubDirectories(dir: string) {
+  return readdirSync(dir)
+    .map(name => join(dir, name))
+    .filter(isDirectory)
+}
+
+function isDirectory(source: string) {
+  return lstatSync(source).isDirectory()
+}
+
+async function gitFetchPrune() {
+  console.log(chalk.dim.gray("Command:") + " " + chalk.yellow("git fetch --prune"))
+  const root = join(process.env.HOME!, "GIT")
+  console.log(chalk.dim.gray("Root:") + " " + chalk.yellow(root))
+  const projects = searchRepositories(root)
+
+  for (const dir of projects) {
+    const n = new Nehemiah(dir)
+    console.log(chalk.dim.gray("Repository:") + " " + chalk.cyan(relative(root, dir)))
+    try {
+      await n.run("git fetch --prune")
+    } catch (error) {
+      console.log(chalk.dim.gray("Repository:") + " " + chalk.cyan(relative(root, dir)) + " " + chalk.red("Failed"))
+    }
+  }
+}
+
+gitFetchPrune()
