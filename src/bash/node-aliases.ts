f18ccd8389e9478907980bd7740b1c54c0c4bd9a
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-09-22 16:49:51 +0200

    Simplify node-aliases with nehemiah

--- src/bash/node-aliases.ts
+++ src/bash/node-aliases.ts
@@ -1,31 +1,19 @@
-import { lstatSync, readdirSync } from "fs"
-import { join } from "path"
+import Nehemiah from "nehemiah"
 
 import { DotScript } from "./DotScript"
 import util from "./util"
 
-export default function generate() {
-  const jsRoot = util.cwd("generated/node")
-  const jsFiles = findJsPartials(jsRoot)
+export default async function generate() {
+  const n = new Nehemiah()
+  const files = await n.find("build/node/*.js")
+  const fileNames = files.map(util.filename)
 
   const sh = new DotScript()
   sh.section("Node Aliases")
 
-  jsFiles.forEach(name => {
+  fileNames.forEach(name => {
     sh.alias(name, `node "$DOT_ROOT/node/${name}.js"`)
   })
 
   return sh.outputTo(__filename)
-}
-
-function findJsPartials(dir: string) {
-  return readdirSync(dir)
-    .map(name => join(dir, name))
-    .filter(isFile)
-    .filter(file => file.endsWith(".js"))
-    .map(util.filename)
-}
-
-function isFile(source: string) {
-  return lstatSync(source).isFile()
 }
