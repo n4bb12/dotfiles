ba17fd8fd3a85f9825b024612558324ef1720c82
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-09-22 17:20:40 +0200

    Make generated bash file more readable

--- src/bash/index.ts
+++ src/bash/index.ts
@@ -8,23 +8,8 @@ const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 
 export default async function generateIndex() {
-  const contents: string[] = []
-
-  for (const name of partials) {
-    const filename = name + ".sh"
-    const srcFile = util.cwd("src/bash-partials", filename)
-    const generatedFile = util.cwd("build/bash-partials", filename)
-
-    if (fs.existsSync(srcFile)) {
-      contents.push(await readScript(srcFile))
-    } else if (fs.existsSync(generatedFile)) {
-      contents.push(await readScript(generatedFile))
-    } else {
-      throw new Error("Bash partial does not exist: " + name)
-    }
-  }
-
-  const combined = contents.join("\n")
+  const contents = await Promise.all(partials.map(readPartial))
+  const combined = combine(contents)
   const outFile = util.cwd("build/bash/index.sh")
 
   return writeFile(outFile, combined, "utf8")
@@ -39,4 +24,52 @@ async function readScript(file: string) {
     .map(line => line.replace(/alias\s+/, "alias "))
     .filter(line => !line.startsWith("#"))
     .join("\n")
+}
+
+function combine(contents: string[]): string {
+  const isAlias = /^alias /
+  const isRequirement = /^require-(?:binary|node-package)[^()]+$/
+  const isOnelineFunction = /^[\w-_]+\(\)\s+{.*?}$/
+
+  const lines = contents
+    .join("\n")
+    .split("\n")
+
+  const remaining = lines
+    .filter(line => {
+      return true
+        && !isOnelineFunction.test(line)
+        && !isAlias.test(line)
+        && !isRequirement.test(line)
+    })
+
+  const onelineFunctions = lines
+    .filter(line => isOnelineFunction.test(line))
+
+  const aliases = lines
+    .filter(line => isAlias.test(line))
+
+  const requirements = lines
+    .filter(line => isRequirement.test(line))
+
+  return [
+    ...remaining, "",
+    ...onelineFunctions.sort(), "",
+    ...aliases.sort(), "",
+    ...requirements.sort(),
+  ].join("\n")
+}
+
+async function readPartial(name: string) {
+  const filename = name + ".sh"
+  const srcFile = util.cwd("src/bash-partials", filename)
+  const generatedFile = util.cwd("build/bash-partials", filename)
+
+  if (fs.existsSync(srcFile)) {
+    return readScript(srcFile)
+  } else if (fs.existsSync(generatedFile)) {
+    return readScript(generatedFile)
+  } else {
+    throw new Error("Bash partial does not exist: " + name)
+  }
 }
