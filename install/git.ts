e98dfa5b6421239f64f11fc1cc43732c6daa822d
abrsil-WittGruppe <abraham.schilling@witt-gruppe.eu>
2019-03-27 13:12:32 +0100

    feat(git): set core.eol

--- install/git.ts
+++ install/git.ts
@@ -12,10 +12,11 @@ export default async () => {
 
   await n.run(`git config --global core.autocrlf false`)
   await n.run(`git config --global core.editor "code --wait"`)
+  await n.run(`git config --global core.eol lf`)
   await n.run(`git config --global diff.tool "vs-code"`)
   await n.run(`git config --global difftool.cmd "code --wait --diff $LOCAL $REMOTE"`)
-  await n.run(`git config --global push.default current`)
-  await n.run(`git config --global push.followTags true`)
   await n.run(`git config --global log.date iso`)
   await n.run(`git config --global pull.rebase true`)
+  await n.run(`git config --global push.default current`)
+  await n.run(`git config --global push.followTags true`)
 }
