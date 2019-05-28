36d793cdcb462819b208308e1d413fbded9fcb83
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-05-28 22:11:26 +0200

    Add dist bin folder to PATH

--- bash-partials/header.sh
+++ bash-partials/header.sh
@@ -4,4 +4,5 @@ if [[ ! -v DOT_ROOT ]]; then
   export DOT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
   export DOT_PATH="$DOT_ROOT/node_modules/.bin:$PATH"
   export SYSTEM_PATH="$PATH"
+  export PATH="$DOT_PATH"
 fi
