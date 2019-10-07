6dcba3d5868008a10bd6007e6155dece2d4644f8
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-10-07 15:52:30 +0200

    Add jq for other tools

--- bash-partials/fx.sh
+++ bash-partials/fx.sh
@@ -11,6 +11,9 @@
 
 require-node-package 'fx'
 
+# just install this for other tools that need it, we're not using it in aliases
+require-node-package 'jq'
+
 alias      scripts='cat package.json | fx .scripts'
 alias         deps='cat package.json | fx .dependencies'
 alias        ddeps='cat package.json | fx .devDependencies'
