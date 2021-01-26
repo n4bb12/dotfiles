0105faeaeeb9ebffaffcdaf8ffcc79090e89d061
Abraham Schilling <AbrahamSchilling@gmail.com>
2021-01-26 18:08:44 +0100

    Update git.ts

--- install/nehemiah/git.ts
+++ install/nehemiah/git.ts
@@ -11,13 +11,14 @@ export default async () => {
   const n = new Nehemiah()
 
   await n.run(`git config --global core.autocrlf false`)
-  await n.run(`git config --global core.editor "code --wait"`)
   await n.run(`git config --global core.eol lf`)
   await n.run(`git config --global core.ignorecase false`)
-  await n.run(`git config --global diff.tool "vs-code"`)
-  await n.run(`git config --global difftool.cmd "code --wait --diff $LOCAL $REMOTE"`)
   await n.run(`git config --global log.date iso`)
-  await n.run(`git config --global pull.rebase true`)
   await n.run(`git config --global push.default current`)
   await n.run(`git config --global push.followTags true`)
+  await n.run(`git config --global fetch.prune true`)
+  await n.run(`git config --global pull.rebase true`)
+  await n.run(`git config --global core.editor 'code --wait'`)
+  await n.run(`git config --global diff.tool 'code'`)
+  await n.run(`git config --global difftool.code.cmd 'code --wait --diff $LOCAL $REMOTE'`)
 }
