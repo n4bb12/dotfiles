bd64c17f4a0f770bf7f7b87c07a46acee49ea99e
Abraham Schilling <AbrahamSchilling@gmail.com>
2021-05-15 02:41:39 +0200

    Extend git config

--- install/config.ps1
+++ install/config.ps1
@@ -49,11 +49,12 @@ pnpm config set store-dir 'D:\pnpm\cache'
 pnpm config set global-dir 'D:\pnpm\global'
 
 git config --global alias.lg 'log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit --date=relative'
+git config --global color.ui auto
+git config --global core.attributesFile ~/.gitattributes
 git config --global core.autocrlf false
 git config --global core.editor 'code --wait'
 git config --global core.eol lf
 git config --global core.excludesfile ~/.gitignore
-git config --global core.attributesFile ~/.gitattributes
 git config --global core.ignorecase false
 git config --global diff.tool 'code'
 git config --global difftool.code.cmd 'code --wait --diff $LOCAL $REMOTE'
