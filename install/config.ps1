593a4bb2cc24833b5b439e3f9b1e8ff10512f671
Abraham Schilling <AbrahamSchilling@gmail.com>
2022-02-22 01:25:06 +0100

    Update install/config.ps1

--- install/config.ps1
+++ install/config.ps1
@@ -25,8 +25,9 @@ Add-To-Path "C:\Program Files\MongoDB\Shell\bin"
 Add-To-Path "C:\Program Files\MongoDB\Tools\100\bin"
 Add-To-Path "C:\Program Files\PostgreSQL\13\bin"
 Add-To-Path "D:\Tools\node-14"
-Add-To-Path "E:\npm\global"
-Add-To-Path "E:\yarn\bin"
+Add-To-Path "D:\Tools\npm\global"
+Add-To-Path "D:\Tools\yarn\bin"
+Add-To-Path 'D:\Tools\pnpm\global'
 
 Set-PSDebug -Trace 1
 
@@ -47,24 +48,24 @@ git config --global push.default current
 git config --global push.followTags true
 git config --global rebase.autosquash true
 
-npm config set cache 'E:\npm\cache'
+npm config set cache 'D:\Tools\npm\cache'
 npm config set color always
 npm config set editor code
 npm config set git-tag-version true
 npm config set msvs_version 2019
-npm config set prefix 'E:\npm\global'
+npm config set prefix 'D:\Tools\npm\global'
 npm config set progress true
 npm config set python python2.7
 npm config set shell bash
 
-yarn config set prefix 'E:\yarn'
-yarn config set cache-folder 'E:\yarn\cache'
-yarn config set global-folder 'E:\yarn\global'
+yarn config set prefix 'D:\Tools\yarn'
+yarn config set cache-folder 'D:\Tools\yarn\cache'
+yarn config set global-folder 'D:\Tools\yarn\global'
 
 npm i -g pnpm
-pnpm config set prefix 'D:\pnpm'
-pnpm config set store-dir 'D:\pnpm\cache'
-pnpm config set global-dir 'D:\pnpm\global'
+pnpm config set prefix 'D:\Tools\pnpm'
+pnpm config set store-dir 'D:\Tools\pnpm\cache'
+pnpm config set global-dir 'D:\Tools\pnpm\global'
 
 Set-PSDebug -Trace 0
 
