644cb784f5033fed65394b4763804dc166c46c7d
Abraham Schilling <AbrahamSchilling@gmail.com>
2022-02-25 00:26:17 +0100

    Add keyboard language to mac setup script

--- install/mac.sh
+++ install/mac.sh
@@ -4,6 +4,14 @@ set -ex
 
 xcode-select --install || true
 
-source "$DIR/git.sh"
-source "$DIR/npm-mac.sh"
-source "$DIR/brew.sh"
+# source "$DIR/git.sh"
+# source "$DIR/npm-mac.sh"
+# source "$DIR/brew.sh"
+
+# Show hidden files
+defaults write com.apple.Finder AppleShowAllFiles true
+
+# Set sign-in keyboard profile to 'German - Standard'
+for home in /Users/*/; do
+  sudo cp -R ${DIR}/../config/Library/* ${home}Library/
+done
