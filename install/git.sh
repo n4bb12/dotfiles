35dd9f16fb69becd4c725d3b55d904beafe7318c
Abraham Schilling <AbrahamSchilling@gmail.com>
2022-02-24 23:57:03 +0100

    Avoid exit 1 when unsetting user.email

--- install/git.sh
+++ install/git.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-git config --global --unset-all user.email
+git config --global --unset-all user.email || true
+git config --global --unset-all user.name || true
 git config --global alias.lg 'log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit --date=relative'
 git config --global color.ui auto
 git config --global core.attributesFile ~/.gitattributes
