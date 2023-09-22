189da813d43a3236476c9b1e7d88c96133b3bbd1
Abraham Schilling <AbrahamSchilling@gmail.com>
2023-09-22 17:05:17 +0200

    Reuse VSCode windows when using it with Git

--- install/git.sh
+++ install/git.sh
@@ -8,19 +8,19 @@ git config --global alias.lg 'log --graph --pretty=format:"%Cred%h%Creset -%C(ye
 git config --global color.ui auto
 git config --global core.attributesFile ~/.gitattributes
 git config --global core.autocrlf false
-git config --global core.editor 'code --wait'
+git config --global core.editor 'code --wait --reuse-window'
 git config --global core.eol lf
 git config --global core.excludesfile ~/.gitignore
 git config --global core.ignorecase false
 git config --global diff.tool vscode
-git config --global difftool.vscode.cmd 'code --wait --diff $LOCAL $REMOTE'
+git config --global difftool.vscode.cmd 'code --wait --reuse-window --diff $LOCAL $REMOTE'
 git config --global fetch.prune true
 git config --global grep.extendRegexp true
 git config --global grep.lineNumber true
 git config --global init.defaultBranch main
 git config --global log.date iso
 git config --global merge.tool vscode
-git config --global mergetool.vscode.cmd 'code --wait $MERGED'
+git config --global mergetool.vscode.cmd 'code --wait --reuse-window $MERGED'
 git config --global pager.branch false
 git config --global pager.grep false
 git config --global pager.log false
