b2ff80f9331b2fc98151e9c384bdfeb713920aa4
Abraham Schilling <AbrahamSchilling@gmail.com>
2024-01-28 15:05:01 +0100

    Update WSL aliases

--- config/~/.aliases/os_wsl.sh
+++ config/~/.aliases/os_wsl.sh
@@ -8,6 +8,15 @@ source /etc/profile.d/bash_completion.sh
 # "$(wslpath -w "$PWD")"'
 PROMPT_COMMAND=${PROMPT_COMMAND:+"$PROMPT_COMMAND; "}'printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"'
 
+# vscode
+export PATH="/mnt/c/Users/ACS/AppData/Local/Programs/Microsoft VS Code/bin:$PATH"
+
+# vcxsrv
+export DISPLAY=$(ip route|awk '/^default/{print $3}'):0.0
+export XCURSOR_SIZE=64
+export GDK_SCALE=0.67
+export GDK_DPI_SCALE=1.5
+
 # host binaries
 alias clip="clip.exe"
 alias explorer="explorer.exe"
