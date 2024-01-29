86171d518dff9d797a647573057c68f56e70340e
Abraham Schilling <AbrahamSchilling@gmail.com>
2024-01-29 14:28:20 +0100

    Update os_wsl.sh

--- config/~/.aliases/os_wsl.sh
+++ config/~/.aliases/os_wsl.sh
@@ -8,9 +8,6 @@ source /etc/profile.d/bash_completion.sh
 # "$(wslpath -w "$PWD")"'
 PROMPT_COMMAND=${PROMPT_COMMAND:+"$PROMPT_COMMAND; "}'printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"'
 
-# vscode
-export PATH="/mnt/c/Users/ACS/AppData/Local/Programs/Microsoft VS Code/bin:$PATH"
-
 # vcxsrv
 export DISPLAY=$(ip route|awk '/^default/{print $3}'):0.0
 export XCURSOR_SIZE=64
